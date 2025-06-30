7e08c82a5c64bf8383092829fe287e42e90fd8d1
hugo-vazquez <ugosan.hv@gmail.com>
2025-06-30 16:52:37 +0200

    update

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+#!/usr/bin/python3
+
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import List, Optional
+import json
+import logging
+
+#Configure logging
+logging.basicConfig(level=logging.INFO)
+
+app = FastAPI()
 
