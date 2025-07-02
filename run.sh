b5aa2c1fda53400eb73ba36443109ae2cfe732f6
hugo-vazquez <ugosan.hv@gmail.com>
2025-07-02 17:13:54 +0200

    run.sh file

---
+++ run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+VENV_DIR="venv"
+
+# Create virtual environment if it doesn't exist
+if [ ! -d "$VENV_DIR" ]; then
+  echo "Creating virtual environment..."
+  python3 -m venv $VENV_DIR
+fi
+
+# Activate the virtual environment
+echo "Activating virtual environment..."
+source $VENV_DIR/bin/activate
+
+# Install dependencies
+echo "Installing dependencies..."
+pip install --upgrade pip
+pip install -r requirements.txt
+
+# Start the FastAPI app
+echo "Starting FastAPI app with Uvicorn..."
+uvicorn main:app --reload
+
