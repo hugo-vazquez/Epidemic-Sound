26a697e3af3428e78c0c5906910d12f70bc0b680
hugo-vazquez <ugosan.hv@gmail.com>
2025-07-03 20:18:00 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# Set environmental variables
-VENV_DIR="venv"
-OKTA_DOMAIN="https://dev-04279224-admin.okta.com"
-OKTA_API_TOKEN="00ta_mfskUB028k6X7EqBrDKWbqboefIFmTlSSUpXw"
+# Set environment variables with export
+export OKTA_DOMAIN="https://dev-04279224-admin.okta.com"
+export OKTA_API_TOKEN="00ta_mfskUB028k6X7EqBrDKWbqboefIFmTlSSUpXw"
 
 # Create virtual environment if it doesn't exist
+VENV_DIR="venv"
 if [ ! -d "$VENV_DIR" ]; then
   echo "Creating virtual environment..."
   python3 -m venv $VENV_DIR
@@ -15,17 +15,14 @@ fi
 echo "Activating virtual environment..."
 source $VENV_DIR/bin/activate
 
-# Install dependencies only if not already satisfied
+# Upgrade pip and install dependencies if needed
 echo "Checking dependencies..."
 pip install --upgrade pip > /dev/null
 
-if ! pip check > /dev/null 2>&1; then
-  echo "Installing missing dependencies..."
-  pip install -r requirements.txt
-else
-  echo "All requirements already satisfied."
-fi
+echo "Installing missing dependencies..."
+pip install -r requirements.txt
 
 # Start the FastAPI app
 echo "Starting FastAPI app with Uvicorn..."
 uvicorn main:app --reload
+
